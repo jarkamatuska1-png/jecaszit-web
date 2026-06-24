433802c9439767cb8e4771fb3a08f171ab0a6db4
Jarka Matuskova <jarkamatuska1@gmail.com>
2026-06-24 16:56:37 +0200

    Odstraněna sekce recenzí

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Hero from "@/components/Hero";
 import ForWho from "@/components/ForWho";
 import About from "@/components/About";
 import Services from "@/components/Services";
-import Testimonials from "@/components/Testimonials";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
@@ -16,7 +15,6 @@ export default function Home() {
         <ForWho />
         <About />
         <Services />
-        <Testimonials />
         <Contact />
       </main>
       <Footer />
